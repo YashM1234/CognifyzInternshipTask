449863b534b47e82ddad4f4f2734f8f615f48573
YashM1234 <yashmungolerpg@gmail.com>
2024-02-23 15:44:20 +0530

    Level-2 Task-3 File Encryption/Decryption...

---
+++ src/main/java/com/cognifyz/level2/task3/util/OutputFilePath.java
@@ -0,0 +1,18 @@
+package com.cognifyz.level2.task3.util;
+
+import java.io.File;
+
+public class OutputFilePath {
+    public static String generateOutputFilePath(String inputFileName, String operation) {
+        File inputFile = new File(inputFileName);
+        String fileNameWithoutExtension = inputFile.getName().replaceFirst("[.][^.]+$", "");
+
+        String extension = "";
+        if (inputFileName.lastIndexOf(".") > 0) {
+            extension = inputFileName.substring(inputFileName.lastIndexOf("."));
+        }
+
+        String outputFileName = fileNameWithoutExtension + "_" + operation + extension;
+        return inputFile.getParent() + File.separator + outputFileName;
+    }
+}
